d02bc6696e4d8914c0180584a3786a870173c787
SirPython <SirPython@users.noreply.github.com>
2019-10-01 18:22:10 -0400

    Create tokenizer.h

---
+++ tokenizer.h
@@ -0,0 +1,44 @@
+#ifndef TOKENIZER
+#define TOKENIZER
+
+#include "gary.h"
+
+#include <stdlib.h>
+
+#define IS_NUMBER(c) (c >= '0' && c <= '9')
+#define IS_LETTER(c) ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+#define IS_OPERATOR(c) (c == '+' || c == '-' || c == '*' || c == '/')
+#define IS_PAREN(c) (c == '(' || c == ')')
+#define IS_QUOTE(c) (c == '"')
+#define IS_BRACE(c) (c == '{' || c == '}')
+
+typedef enum {
+    NUMBER,
+    LETTER,
+    OPERATOR,
+    PAREN,
+    QUOTE,
+    BRACE
+} TokenType;
+
+typedef struct {
+    TokenType name;
+    char *value;
+    int value_length;
+} Token;
+
+typedef struct {
+    Token *tokens;
+    size_t used;
+    size_t size;
+} TokenArray;
+
+void TokenArray_init(TokenArray *ta, size_t size);
+
+void TokenArray_add(TokenArray *ta, Token *t);
+
+void TokenArray_clear(TokenArray *ta);
+
+void tokenize(char *code, TokenArray *ta);
+
+#endif
