5dd3a8e84175f52a6ed8f8b5ba80bfde6bca4f74
SirPython <SirPython@users.noreply.github.com>
2019-10-02 10:38:44 -0400

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -6,59 +6,69 @@
 #include <string.h>
 
 void tokenize(char *code, TokenArray *ta) {
-    while(*(code++) != '\0') {
+    while(*code != '\0') {
         Token t;
 
         if(IS_NUMBER(*code)) {
-            puts("number");
-
             t.name = NUMBER;
             t.value = code;
             t.value_length = 0;
 
-            while(IS_NUMBER(*(code++))) {
+            while(IS_NUMBER(*code)) {
                 t.value_length++;
+                code++;
             }
 
             TokenArray_add(ta, &t);
         } else
         if(IS_LETTER(*code)) {
-            puts("letter");
-
-            t.name = LETTER;
+            t.name = NAME;
             t.value = code;
             t.value_length = 0;
 
-            while(IS_LETTER(*(code++))) {
+            while(IS_LETTER(*code)) {
                 t.value_length++;
+                code++;
             }
 
             TokenArray_add(ta, &t);
 
         } else
         if(IS_OPERATOR(*code)) {
-            puts("operator");
-
             t.name = OPERATOR;
             t.value = code;
             t.value_length = 1;
             TokenArray_add(ta, &t);
         } else
         if(IS_PAREN(*code)) {
-            puts("parenthesis");
-
             t.name = PAREN;
             t.value = code;
             t.value_length = 1;
             TokenArray_add(ta, &t);
         } else
         if(IS_QUOTE(*code)) {
+            t.name = STRING;
+            t.value = code;
+            t.value_length = 0;
 
+            while(!IS_QUOTE(*code)) {
+                t.value_length++;
+                code++;
+            }
+
+            TokenArray_add(ta, &t);
+        } else
+        if(IS_BRACE(*code)) {
+            t.name = BRACE;
+            t.value = code;
+            t.value_length = 1;
+            TokenArray_add(ta, &t);
         }
         else {
-            puts("other");
             // ignore
         }
+
+        code++;
     }
 }
 
