04db30e0690cecf86ebb001e13d4aea978062712
SirPython <SirPython@users.noreply.github.com>
2019-10-03 21:18:43 -0400

    Update parser.h

--- parser.h
+++ parser.h
@@ -3,8 +3,23 @@
 
 #include "gary.h"
 
+/* Parameters could be a linked list */
+typedef enum {
+    NumberLiteral,
+    StringLiteral,
+    CallExpression,
+    DefinitionExpression
+} ASTNodeType;
+
+/**
+ * token --> the data
+ * next --> sequentially the next node
+ * inner --> the start of an inner of ASTNodes
+ */
 typedef struct {
-    
-} ASTNode
+    struct Token *token;
+    struct ASTNode *next;
+    struct ASTNode *inner;
+} ASTNode;
 
 #endif
