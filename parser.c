7091ee7bdcf5586d63d698c55b8ad942a27a3d1c
SirPython <SirPython@users.noreply.github.com>
2019-10-03 21:18:27 -0400

    Update parser.c

--- parser.c
+++ parser.c
@@ -1,5 +1,57 @@
 #include "gary.h"
+#include "parser.h"
 
-void generate_ast(TokenArray *ta) {
-    
+#include <string.h>
+
+/**
+ * When calling this, pass the top most node and token;
+ */
+void walk_ast(ASTNode *n, Token *t, int i) {
+    switch(t->name) {
+        case NAME: {
+            ASTNode node;
+
+            char *value = malloc(t.value_length);
+            memcpy(value, t.value, t.value_length);
+
+            break;
+        }
+        case OPERATOR:
+
+
+            break;
+        case PAREN:
+            break;
+        
+        /* There's gotta be a simpler way to do this. */
+        case NUMBER: {
+            node.type = NumberLiteral;
+            node.value = t.value;
+            node.value_length = t.value_length;
+
+            break;
+        }
+        case STRING: {
+            node.type = StringLiteral;
+            node.value = t.value;
+            node.value_length = t.value_length;
+
+            break;
+        }
+        case BRACE:
+            break;
+        
+        case COLON: {
+            node.type = DefinitionExpression;
+            node.value = last.value;
+
+            break;
+        }
+    }
+
+    last = t;
+}
+
+void walk_ast(ASTNode *ast) {
+
 }
