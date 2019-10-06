c8f6a3d5b79d14ed370873f70dfdae7234063205
SirPython <SirPython@users.noreply.github.com>
2019-10-06 10:14:05 -0400

    Update parser.h

--- parser.h
+++ parser.h
@@ -16,10 +16,37 @@ typedef enum {
  * next --> sequentially the next node
  * inner --> the start of an inner of ASTNodes
  */
-typedef struct {
+/*struct ASTNode {
     struct Token *token;
     struct ASTNode *next;
     struct ASTNode *inner;
-} ASTNode;
+};*/
+
+
+struct ASTNodeArray {
+    struct ASTNode *nodes;
+    size_t used;
+    size_t size;
+};
+
+void ASTNodeArray_init(struct ASTNodeArray *ta);
+
+void ASTNodeArray_add(struct ASTNodeArray *ta, struct ASTNode *n);
+
+void ASTNodeArray_clear(struct ASTNodeArray *ta);
+
+void ASTNode_print(struct ASTNode *node);
+
+void ASTNodeArray_print(struct ASTNodeArray *na);
+
+struct ASTNode {
+    struct Token *token; // used for literals
+    struct ASTNodeArray inner; // used for expressions
+    // bruh shouldn't this ^ be a pointer? i can't get it to work that way tho
+};
+
+void generate_ast(struct ASTNodeArray *na, struct TokenArray *ta);
+
+void walk_ast(struct ASTNodeArray *na, struct Token *token, struct TokenArray *ta, int i);
 
 #endif
