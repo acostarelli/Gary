85c6051b6ec21912ef494da69d3dcd7eb677bfc0
SirPython <SirPython@users.noreply.github.com>
2019-10-11 20:06:06 -0400

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -1,130 +1,85 @@
+#ifndef TOKENIZER
+#define TOKENIZER
+
 #include "gary.h"
-#include "tokenizer.h"
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-void tokenize(char *code, struct TokenArray *ta) {
-    int current_depth = 0;
+#define IS_NUMBER(c) (c >= '0' && c <= '9')
+#define IS_LETTER(c) ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+#define IS_OPERATOR(c) (c == '+' || c == '-' || c == '*' || c == '/')
+#define IS_PAREN(c) (c == '(' || c == ')')
+#define IS_QUOTE(c) (c == '"')
+#define IS_BRACE(c) (c == '{' || c == '}')
+#define IS_COLON(c) (c == ':')
+#define IS_WHITESPACE(c) (c == ' ' || c == '\n')
 
-    while(*code != '\0') {
-        struct Token t;
+/*typedef enum {
+    NUMBER,
+    NAME,
+    OPERATOR,
+    PAREN,
+    QUOTE,
+    BRACE,
+    STRING,
+    COLON
+} TokenType;
 
-        /* Template code; to be overridden as needed */
-        t.type = LITERAL;
-        t.value = code;
-        t.value_length = 1;
-        t.depth = current_depth;
+struct Token {
+    TokenType name;
+    char *value;
+    int value_length;
+};*/
 
-        /*if(IS_WHITESPACE(c)) {
-            continue;
-        }*/
+/**
+ * LITERAL -> a name or an actual value (string, number)
+ * EXPRESSION -> anything involving a series of expressions
+ * and literals with some sort of operation being done.
+ */
+enum TokenType {
+    LITERAL,
+    EXPRESSION
+};
 
-        /* Literals */
-        if(IS_NUMBER(*code)) {
-            while(IS_NUMBER(*(code+1))) {
-                t.value_length++;
-                code++;
-            }
-            TokenArray_add(ta, &t);
-        } else
-        if(IS_LETTER(*code)) {
-            while(IS_LETTER(*(code+1))) {
-                t.value_length++;
-                code++;
-            }
-            TokenArray_add(ta, &t);
-        } else
-        if(IS_QUOTE(*code)) {
-            while(!IS_QUOTE(*(code+1))) {
-                t.value_length++;
-                code++;
-            }
-            t.value_length++; // TODO Condense this
-            code++; // so that we don't pick it up the next time
-            TokenArray_add(ta, &t);
-        } else {
-            /* Expressions. TODO: Factor */
-            t.type = EXPRESSION;
-            switch(*code) {
-                case '(':
-                case '{': {
-                    current_depth++;
+/**
+ * The actual token value exists between
+ *      *value --> *(value + value_length - 1)
+ *
+ * The depth is an indicator for grouping whichever
+ * Tokens go together under an expression. For example,
+ * a math expression at depth 0 is going to have
+ * two Tokens at depth 1: the operand Tokens.
+ */
+struct Token {
+    enum TokenType type;
+    char *value;
+    int value_length;
+    int depth;
+};
 
-                    TokenArray_add(ta, &t);
-                    break;
-                }
-                case ')':
-                case '}': {
-                    current_depth--; // can ignore closing expression markers
-                    break;
-                }
-                case '+':
-                case '-':
-                case '*':
-                case '/':
-                case '@':
-                case ':': {
-                    current_depth++;
+/*struct Handler {
+    int (*recognizer)(char *);
+    void (*generator)(struct Token*);
+}*/
 
-                    struct Token last = TokenArray_pop(ta);
-                    last.depth = current_depth;
+struct TokenArray {
+    struct Token *tokens;
+    size_t used;
+    size_t size;
+};
 
-                    TokenArray_add(ta, &t);
-                    TokenArray_add(ta, &last);
+struct Token TokenArray_pop(struct TokenArray *ta);
 
-                    current_depth--;
-                    break;
-                }
-            }
-        }
+void TokenArray_init(struct TokenArray *ta);
 
-        code++;
-    }
-}
+void TokenArray_add(struct TokenArray *ta, struct Token *t);
 
-struct Token TokenArray_pop(struct TokenArray *ta) {
-    struct Token ret = ta->tokens[ta->used-1];
-    //free(&ta->tokens[ta->used-1]);
-    ta->used = ta->used - 1;
+void TokenArray_clear(struct TokenArray *ta);
 
-    return ret;
-}
+void tokenize(char *code, struct TokenArray *ta);
 
-void TokenArray_init(struct TokenArray *ta) {
-    ta->tokens = malloc(1 * sizeof(struct Token));
-    ta->used = 0;
-    ta->size = 1;
-}
+void TokenArray_print(struct TokenArray *ta);
 
-void TokenArray_add(struct TokenArray *ta, struct Token *t) {
-    if(ta->used == ta->size) {
-        ta->size = ta->size * 2;
-        ta->tokens = realloc(ta->tokens, ta->size * sizeof(struct Token));
-    }
+void Token_print(struct Token *t);
 
-    ta->tokens[ta->used++] = *t;
-}
-
-void TokenArray_clear(struct TokenArray *ta) {
-    free(ta->tokens);
-    ta->tokens = NULL;
-
-    ta->used = 0;
-    ta->size = 0;
-}
-
-void TokenArray_print(struct TokenArray *ta) {
-    for(int i = 0; i < ta->used; i++) {
-        printf("%d: ", i);
-        Token_print(&ta->tokens[i]);
-        puts("\n");
-    }
-}
-
-void Token_print(struct Token *t) {
-    char *text = malloc(t->value_length);
-    memcpy(text, t->value, t->value_length);
-    printf("%s depth: %d", text, t->depth);
-}
+#endif
