0f38201963a9511b0f8e2db4d2a79429d4a5555a
SirPython <SirPython@users.noreply.github.com>
2019-10-11 20:05:53 -0400

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -6,76 +6,90 @@
 #include <string.h>
 
 void tokenize(char *code, struct TokenArray *ta) {
+    int current_depth = 0;
+
     while(*code != '\0') {
         struct Token t;
 
-        if(IS_NUMBER(*code)) {
-            t.name = NUMBER;
-            t.value = code;
-            t.value_length = 1;
+        /* Template code; to be overridden as needed */
+        t.type = LITERAL;
+        t.value = code;
+        t.value_length = 1;
+        t.depth = current_depth;
 
+        /*if(IS_WHITESPACE(c)) {
+            continue;
+        }*/
+
+        /* Literals */
+        if(IS_NUMBER(*code)) {
             while(IS_NUMBER(*(code+1))) {
                 t.value_length++;
                 code++;
             }
-
             TokenArray_add(ta, &t);
         } else
         if(IS_LETTER(*code)) {
-            t.name = NAME;
-            t.value = code;
-            t.value_length = 1;
-
             while(IS_LETTER(*(code+1))) {
                 t.value_length++;
                 code++;
             }
-
-            TokenArray_add(ta, &t);
-
-        } else
-        if(IS_OPERATOR(*code)) {
-            t.name = OPERATOR;
-            t.value = code;
-            t.value_length = 1;
-            TokenArray_add(ta, &t);
-        } else
-        if(IS_PAREN(*code)) {
-            t.name = PAREN;
-            t.value = code;
-            t.value_length = 1;
             TokenArray_add(ta, &t);
         } else
         if(IS_QUOTE(*code)) {
-            t.name = STRING;
-            t.value = code;
-            t.value_length = 1;
-
             while(!IS_QUOTE(*(code+1))) {
                 t.value_length++;
                 code++;
             }
+            t.value_length++; // TODO Condense this
+            code++; // so that we don't pick it up the next time
+            TokenArray_add(ta, &t);
+        } else {
+            /* Expressions. TODO: Factor */
+            t.type = EXPRESSION;
+            switch(*code) {
+                case '(':
+                case '{': {
+                    current_depth++;
 
-            TokenArray_add(ta, &t);
-        } else
-        if(IS_BRACE(*code)) {
-            t.name = BRACE;
-            t.value = code;
-            t.value_length = 1;
-            TokenArray_add(ta, &t);
-        } else
-        if(IS_COLON(*code)) {
-            t.name = COLON;
-            t.value = code;
-            t.value_length = 1;
-            TokenArray_add(ta, &t);
-        }
-        else {
-            // ignore
+                    TokenArray_add(ta, &t);
+                    break;
+                }
+                case ')':
+                case '}': {
+                    current_depth--; // can ignore closing expression markers
+                    break;
+                }
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '@':
+                case ':': {
+                    current_depth++;
+
+                    struct Token last = TokenArray_pop(ta);
+                    last.depth = current_depth;
+
+                    TokenArray_add(ta, &t);
+                    TokenArray_add(ta, &last);
+
+                    current_depth--;
+                    break;
+                }
+            }
         }
 
         code++;
     }
+}
+
+struct Token TokenArray_pop(struct TokenArray *ta) {
+    struct Token ret = ta->tokens[ta->used-1];
+    //free(&ta->tokens[ta->used-1]);
+    ta->used = ta->used - 1;
+
+    return ret;
 }
 
 void TokenArray_init(struct TokenArray *ta) {
@@ -112,5 +126,5 @@ void TokenArray_print(struct TokenArray *ta) {
 void Token_print(struct Token *t) {
     char *text = malloc(t->value_length);
     memcpy(text, t->value, t->value_length);
-    printf("%s", text);
+    printf("%s depth: %d", text, t->depth);
 }
