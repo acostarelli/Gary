6a60bb2cbf4548550ef437a50a82fe0c5ef604a5
SirPython <SirPython@users.noreply.github.com>
2019-10-12 21:59:33 -0400

    refactoring

--- compiler.h
+++ compiler.h
@@ -1,17 +1,6 @@
 #ifndef COMPILER
 #define COMPILER
 
-#include <stdbool.h>
-
-#define IS_ALPHA(c) ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-
-void print_literal(char *c, bool (*condition)(char *), FILE *out);
-
-void compile(char *code, FILE *out);
-
-char *tokenizer(char *c, bool (*condition)(char));
-
-bool is_string(char c);
-bool is_number(char c);
+void compile(char *c, FILE *out);
 
 #endif
