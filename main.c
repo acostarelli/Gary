6e7be1803480a2c6ad69f9e05a8f1e5f90ec8c92
SirPython <SirPython@users.noreply.github.com>
2019-10-12 21:57:47 -0400

    will worry about the stdlib later

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "gary.h"
 #include "compiler.h"
+#include "generator.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -8,9 +9,6 @@
 int main(void) {
     FILE *code_fp = fopen("./00test.gary", "r");
     char *code = load_file(code_fp);
-
-    FILE *stdlib_fp = fopen("./garystdlib.asm", "r");
-    char *stdlib = load_file(stdlib_fp);
 
     compile(code, stdout);
 
