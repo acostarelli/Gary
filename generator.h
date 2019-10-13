c337057f4379a91a880167f696fd044ab9986b61
SirPython <SirPython@users.noreply.github.com>
2019-10-13 13:13:25 -0400

    Update generator.h

--- generator.h
+++ generator.h
@@ -13,11 +13,27 @@ char *print_token(char *c, bool (*condition)(char), FILE *out);
  */
 char *print_literal(char *c, bool (*condition)(char), FILE *out);
 
+char *print_call_expression(char *c, FILE *out);
 
-char *print_string_literal(char *c, bool (*condition)(char), FILE *out);
+char *print_symbol_literal(char *c,  FILE *out);
 
-char *print_call_expression(char *c, bool(*condition)(char), FILE *out);
+char *print_parameter_block(char *c, FILE *out);
 
+char *print_function_expression(char *c, FILE *out);
+
+struct ParameterBlock {
+    char **parameters;
+    size_t used;
+    size_t size;
+};
+void ParameterBlock_init(struct ParameterBlock *pb);
+void ParameterBlock_add(struct ParameterBlock *pb, char *parameter);
+
+/**
+ * Returns the proper name for function symbol as found in
+ * the standard library.
+ */
+char *symbol_lookup(char *sym);
 
 bool is_string(char c);
 bool is_number(char c);
