62645b7b390addc6c73b9d69184de98087f6d35e
SirPython <SirPython@users.noreply.github.com>
2019-10-16 14:51:24 -0400

    might need this stack

--- compiler.h
+++ compiler.h
@@ -3,4 +3,17 @@
 
 void compile(char *c, FILE *out);
 
+char *print_compiled(char *c, FILE *out);
+
+struct Stack {
+    int data[32];
+    int length;
+};
+
+void Stack_init(struct Stack *s);
+
+void Stack_push(struct Stack *s, int n);
+
+int Stack_pop(struct Stack *s);
+
 #endif
