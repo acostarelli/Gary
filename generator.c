572f7819051769da44decfb4d00dd645f336ae70
SirPython <SirPython@users.noreply.github.com>
2019-10-16 14:51:46 -0400

    getting some error with pop parameters

--- generator.c
+++ generator.c
@@ -49,21 +49,25 @@ char *print_call_expression(char *c, FILE *out) {
 }
 
 char *print_function_expression(char *c, FILE *out) {
-    fprintf(out, )
+    //fprintf(out, )
     //// NEED ANOTHER STACK STRUCTURE IN ORDER TO HAVE THE 
+    return c;
 }
 
 char *print_parameter_block(char *c, FILE *out) {
     int sub_id = uid();
     char *ret;
 
-    struct ParameterBlock pb;
-    ParameterBlock_init(&pb);
+    /*struct ParameterBlock pb;
+    ParameterBlock_init(&pb);*/
+
+    char *pb[5];//malloc(5 * sizeof(char *));
+    int pb_i = 0;
 
     int jump_id = uid();
     fprintf(out,
         "jmp _skip%d\n"
-        "__sub%d_return: dw 0",
+        "__sub%d_return: dw 0\n",
     jump_id, sub_id);
 
     while(*c != ':') {
@@ -74,20 +78,41 @@ char *print_parameter_block(char *c, FILE *out) {
 
         fprintf(out, "__sub%d_", sub_id);
         char *symbol_end = print_token(c, is_symbol, out);
-        fputs(": dw 0", out);
+        fputs(": dw 0\n", out);
+
+
+        // IS IT A NULL POINTER THING?
+        printf("%c %c %d: %d %d\n", *symbol_start, *symbol_end, ((int)symbol_end - (int)symbol_start)+1, (int)symbol_start, (int)symbol_end);
+
 
         /* For the pop statements */
-        char *token;
-        memcpy(token, symbol_start, symbol_end - symbol_start);
-        ParameterBlock_add(&pb, token);
+        //char *token;
+        memcpy((pb+(pb_i*sizeof(char*))), symbol_start, ((int)symbol_end - (int)symbol_start) + 1);
+        pb_i++;
+
+        printf("mother fucker please %s\n", pb[0]);
+
+        puts("don't make it here");
+
+        //printf("here %s\n", pb[pb_i - 1]);
+
+        /*memcpy(token, symbol_start, symbol_end - symbol_start);*/
+        //fprintf(out, "Arrgh! Ye token ought be %s!\n", token);
+
+        //ParameterBlock_add(&pb, token);
+        //pb[i] = token;
 
         c = symbol_end + 1; // don't double count the end of a symbol
     }
 
-    fprintf(out, "_skip%d:", jump_id);
-    for(int i = pb.used; i < 0; i--) {
-        //pb.parameters[i]
-        fprintf(out, "pop %s\n", pb.parameters[i-1]);
+    fprintf(out, "_skip%d:\n", jump_id);
+    // okay so it seems to me that repl.it goes full retard when you have
+    // loops and structure values. If I change this < to a > as it should be,
+    // the code fails at different spots every time
+    for(; pb_i > 0; pb_i--) {
+        //fprintf(out, "pop %s\n", pb[pb_i-1]);
+        //fprintf(out, "okay %s\n", pb[pb_i-1]);
+        printf("my gawd %s\n", pb[0]);
     }
 
     return c;
@@ -143,7 +168,11 @@ bool is_symbol(char c) {
     return
         (c >= 'A' && c <= 'Z') || 
         (c >= 'a' && c <= 'z') ||
-        (c == '@');
+        (c == '@') ||
+        (c == '+') ||
+        (c == '-') ||
+        (c == '*') ||
+        (c == '/');
 };
 
 void ParameterBlock_init(struct ParameterBlock *pb) {
