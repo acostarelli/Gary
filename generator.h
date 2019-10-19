8740877c7d88bdeba5550ff40ad5deaddd52ee8c
SirPython <SirPython@users.noreply.github.com>
2019-10-19 10:29:07 -0400

    handles scope

--- generator.h
+++ generator.h
@@ -6,20 +6,22 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-char *print_token(char *c, bool (*condition)(char), FILE *out);
+#define PTR_RANGE(start, end) (((int)end - (int)start) + 1)
+
+char *print_token(char *c, bool (*condition)(char), int subid, FILE *out);
 
 /**
  * Returns what ought be the new code pointer so that code isn't reread.
  */
-char *print_literal(char *c, bool (*condition)(char), FILE *out);
+ char *print_literal(char *c, bool (*condition)(char), int subid, FILE *out);
 
-char *print_call_expression(char *c, FILE *out);
+char *print_call_expression(char *c, int subid, FILE *out);
 
-char *print_symbol_literal(char *c,  FILE *out);
+char *print_symbol_literal(char *c, int subid, FILE *out);
 
 char *print_parameter_block(char *c, FILE *out);
 
-char *print_function_expression(char *c, FILE *out);
+char *print_function_expression(char *c, int subid, FILE *out);
 
 struct ParameterBlock {
     char **parameters;
@@ -27,7 +29,7 @@ struct ParameterBlock {
     size_t size;
 };
 void ParameterBlock_init(struct ParameterBlock *pb);
-void ParameterBlock_add(struct ParameterBlock *pb, char *parameter);
+void ParameterBlock_add(struct ParameterBlock *pb, char *start, size_t size);
 
 /**
  * Returns the proper name for function symbol as found in
