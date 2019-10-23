853392570994ed2d5ae354be27bb420a46c0500b
SirPython <SirPython@users.noreply.github.com>
2019-10-23 12:47:05 -0400

    Update garystdlib.asm

--- garystdlib.asm
+++ garystdlib.asm
@@ -1,19 +1,27 @@
 ; maybe just create a shell script that runs the main.c program and then
 ; just cats this file into the output. Then the shell script could also
 ; assemble the code.
-
 jmp __gary_end
 
 __gary_assignment:
+    ret
 
 __gary_addition:
+    ret
 
 __gary_subtraction:
+    ret
 
 __gary_multiplication:
+    ret
 
 __gary_division:
+    ret
 
 __gary_exponent:
+    ret
+
+__gary_print:
+    ret
 
 __gary_end:
