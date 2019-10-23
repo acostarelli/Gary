e161d0fae01be2c3cda2e67527dbbb9a603ce5fa
SirPython <SirPython@users.noreply.github.com>
2019-10-22 21:42:17 -0400

    temporary until better organization

---
+++ lookup.h
@@ -0,0 +1,22 @@
+#ifndef LOOKUP
+#define LOOKUP
+
+#include "compiler.h"
+#include "generator.h"
+
+#include <stdbool.h>
+
+// TODO repetitive from print_symbol
+bool SymbolList_contains_next_symbol(struct DynStrArray *sl, char *c);
+
+/**
+ * Returns the proper name for function symbol as found in
+ * the standard library.
+ */
+char *symbol_lookup(char *sym);
+
+bool is_string(char c);
+bool is_number(char c);
+bool is_symbol(char c);
+
+#endif
