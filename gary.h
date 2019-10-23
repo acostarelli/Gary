d0a07fa6c797604738f8098f39604fff5bd1390b
SirPython <SirPython@users.noreply.github.com>
2019-10-22 21:41:07 -0400

    reorganization

--- gary.h
+++ gary.h
@@ -2,11 +2,31 @@
 #define GARY
 
 #include <stdio.h>
+#include <stdbool.h>
 
 int get_file_size(FILE *fp);
 
 char *load_file(FILE *fp);
 
 int uid();
+
+void stringcpy(char *dst, char *src, size_t size);
+
+struct Stack {
+    int data[32];
+    int length;
+};
+void Stack_init(struct Stack *s);
+void Stack_push(struct Stack *s, int n);
+int Stack_pop(struct Stack *s);
+
+struct DynStrArray {
+    char **items;
+    size_t used;
+    size_t size;
+};
+void DynStrArray_init(struct DynStrArray *dsa);
+void DynStrArray_add(struct DynStrArray *dsa, char *item);
+bool DynStrArray_contains(struct DynStrArray *dsa, char *str);
 
 #endif
